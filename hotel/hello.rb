2d915b5d2a70f92326316b82c04a4bd3ed963ba0
sunchen115 <sunchen115work@gmail.com>
2014-07-08 09:46:23 +0800

    delete fiel

--- hotel/hello.rb
+++
@@ -1,4 +0,0 @@
-puts "hello world"
-puts "hahsa"
-puts "modify 4 we"
-
--- hotel/hotel/main.rb
+++
@@ -1,9 +0,0 @@
-require './hotel_system'
-
-hotel_sys = HotelSystem.new("./hotels_config.yml")
-
-File.open("./input_list", "r") do |file|
-  while line=file.gets
-    puts hotel_sys.find_cheapes_hotel(line)
-  end
-end
